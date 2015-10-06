ac0463e0bc2017d7edd251d19f9177c39fdbc74f
Solyankin <a.n.solyankin@gmail.com>
2015-10-06 10:24:17 +0300

    Initialization

--- uisteps-core/src/main/java/com/qantium/uisteps/core/browser/pages/UIElement.java
+++ uisteps-core/src/main/java/com/qantium/uisteps/core/browser/pages/UIElement.java
@@ -1,7 +1,10 @@
 package com.qantium.uisteps.core.browser.pages;
 
 import com.qantium.uisteps.core.then.Then;
+import java.util.List;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.internal.WrapsElement;
 import ru.yandex.qatools.htmlelements.element.TypifiedElement;
 
 /**
@@ -13,13 +16,19 @@ public abstract class UIElement extends TypifiedElement implements UIObject {
     public UIElement(WebElement wrappedElement) {
         super(wrappedElement);
     }
-    
-    
+
+    public <T extends WrapsElement> T find(Class<T> uiObject, By by) {
+        return inOpenedBrowser().find(uiObject, by, getWrappedElement());
+    }
+
+    public <T extends WrapsElement> List<T> findAll(Class<T> uiObject, By by) {
+        return inOpenedBrowser().findAll(uiObject, by, getWrappedElement());
+    }
+
     public String getText() {
         return inOpenedBrowser().getTextFrom(this);
     }
 
-    
     public Object click() {
         inOpenedBrowser().click(this);
         return null;
@@ -34,15 +43,15 @@ public abstract class UIElement extends TypifiedElement implements UIObject {
         inOpenedBrowser().clickOnPoint(this, x, y);
         return null;
     }
-    
+
     protected <T extends UIObject> Then<T> then(Class<T> uiObject) {
         return inOpenedBrowser().then(uiObject);
     }
-    
+
     protected <T> Then<T> then(T value) {
         return inOpenedBrowser().then(value);
     }
-    
+
     protected <T extends UIObject> T onDisplayed(Class<T> uiObject) {
         return inOpenedBrowser().onDisplayed(uiObject);
     }
